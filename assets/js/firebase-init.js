fa45ff3d7c07fdb5f1151cc0bf6f15374d32a90e
infotech banda larga <coodenext@gmail.com>
2025-04-09 17:04:02 -0300

    alteração de pasta

---
+++ assets/js/firebase-init.js
@@ -0,0 +1,18 @@
+// assets/js/firebase-init.js
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "SUA_API_KEY",
+  authDomain: "SUA_AUTH_DOMAIN",
+  projectId: "SEU_PROJECT_ID",
+  storageBucket: "SEU_STORAGE_BUCKET",
+  messagingSenderId: "SEU_SENDER_ID",
+  appId: "SEU_APP_ID"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { auth };
